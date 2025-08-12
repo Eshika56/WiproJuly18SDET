06de2f0b4e787da25df1d9ec980eee617fea2425
Eshika56 <eshikabose6@gmail.com>
2025-08-12 09:50:46 +0530

    Updared file content

--- hellobangalore.java
+++ hellobangalore.java
@@ -3,7 +3,7 @@ package hello;
 public class hellobangalore {
 
 	public static void main(String[] args) {
-		System.out.println ("Hello how are you?");
+		System.out.println ("Hello how are you? I'm in Bangalore");
 	}
 
 	}
